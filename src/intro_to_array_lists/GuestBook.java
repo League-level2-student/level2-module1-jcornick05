649c47f380c75cd136ab891404041f07b44d387b
U have'nt <whaaqaaaidchbfgvruihgiudfgiufhjifo@gmail.com>
2018-12-07 16:57:04 -0800

    FindTheHiddenButton, GuestBook IP

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,66 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.imageio.ImageTypeSpecifier;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	JFrame f;
+	JPanel p;
+	JButton a;
+	JButton b;
+	ArrayList x = new ArrayList();
+	public static void main(String[] args) {
+		GuestBook c = new GuestBook();
+		c.play();
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+		
+	}
+	void play() {
+		f = new JFrame();
+		p = new JPanel();
+		a = new JButton();
+		b = new JButton();
+	b.setText("View Names");
+	a.setText("Add Name");
+	f.add(p);
+	p.add(a);
+	p.add(b);
+	a.addActionListener(this);
+	a.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonClicked = (JButton)e.getSource();
+		if(buttonClicked==a) {
+			String i = JOptionPane.showInputDialog("Enter name here");
+			x.add(i);
+		}
+		if(buttonClicked==b) {
+			JOptionPane.showMessageDialog(null, x);
+		}
+		
+	}
+	
+	
+
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
+	     
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	
+	
+	
 }
